8905ed4a5ee7bd61c35935f1911f69bd79a5c46f
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2022-03-18 20:26:26 +0100

    Display API key

--- payment-monitor-backend/monitor-backend/dashboard/views.py
+++ payment-monitor-backend/monitor-backend/dashboard/views.py
@@ -1,10 +1,11 @@
 from api.models import Payment
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from api.models import User
 # Create your views here.
 
 
 @login_required
 def dashboard(request):
     payments = Payment.objects.filter(user=request.user)
-    return render(request, "dashboard.html", {'payments': payments})
+    return render(request, "dashboard.html", {'payments': payments, 'apikey': request.user.apikey})
