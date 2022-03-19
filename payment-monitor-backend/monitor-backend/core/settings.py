9a1694e36947990381bbde11cb95e50cd70a3ae3
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2022-03-19 22:06:56 +0100

    Make dashboard the base url

--- payment-monitor-backend/monitor-backend/core/settings.py
+++ payment-monitor-backend/monitor-backend/core/settings.py
@@ -34,7 +34,7 @@ if SECRET_KEY == None:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost',
+ALLOWED_HOSTS = ['localhost', 'api.localhost',
                  'pmonitor.golem.network', 'api.pmonitor.golem.network']
 
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
