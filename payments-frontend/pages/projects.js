4152dae0614c77b9c459469b26d32ac9d9e8dc38
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2022-04-21 21:56:27 +0200

    Fix wrong url

--- payments-frontend/pages/projects.js
+++ payments-frontend/pages/projects.js
@@ -43,7 +43,7 @@ function Page({ projects }) {
 
   const createProject = async (event) => {
     event.preventDefault() // don't redirect the page
-    const save_project = await postData("http://api.localhost/v1/projects", "post", { name: event.target.name.value })
+    const save_project = await postData(process.env.API_BASE + "v1/projects", "post", { name: event.target.name.value })
     if (save_project.status == 201) {
       const returned_data = await save_project.json()
       project.push(returned_data)
