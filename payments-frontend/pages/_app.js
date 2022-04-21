1c66f2e033669f791eee4c389b8516a43adccd22
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2022-04-21 21:36:56 +0200

    Working Authentication!!!!

--- payments-frontend/pages/_app.js
+++ payments-frontend/pages/_app.js
@@ -1,11 +1,14 @@
 import "../styles/globals.css"
 import Navbar from "../components/Navbar"
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <div>
-      <Navbar></Navbar>
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Navbar></Navbar>
+        <Component {...pageProps} />
+      </SessionProvider>
     </div>
   )
 }
